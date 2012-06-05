bef41f605ba6504660a80cc3a040333a171ddfdb
Damien Cornu <damienc.cornu@gmail.com>
2012-06-05 13:54:28 +0200

    Display colored countries on the map

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 ## Description
 Dataviz on campings localization and standing
 
-## Where ?
+## Where?
 This project is not available online yet (but you can still download it and run it on your own server).
 
-## Why ?
+## Why?
 It's a student project made for [HÉTIC](http://www.hetic.net/), we had to make a dataviz on the subject "trips", we choose to talk about campings because whereas there is an economic crisis campings are still cheap and standings have envolved there.
 
-## Who ?
+## Who?
 * CORNU Damien
 * DELAIR Émilie
 * RIVAT Aldric
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,47 @@
 $(function() {
   
-  // Load map
-  
-  
+  // Loading 
   var ds = new Miso.Dataset({
     url : 'data/camping.csv',
     delimiter : ';',
     resetOnFetch : true
   });
-
-  ds.fetch({
-    success : function() {
-      console.log(this.groupBy('REG', ['CPGE12']))
-    }
-  });
+  
+  initDataviz();
+  
+  function initDataviz(){
+    updateMap(2012);
+  }
+  
+  function updateMap(year){
+    ds.fetch({
+      success : function() {
+        var datas = this.where({
+          columns: ['REG','CPG12'],
+          rows: function(row) {
+            return row.ANNEE == year;
+          }
+        });
+        _.each(datas.toJSON(), function(val){
+          console.log("val.REG : ");
+          console.log(val.REG);
+          switch(true){
+            case (val.CPG12 < 300):
+              document.getElementById("mapsvg").contentDocument.getElementById("reg-"+val.REG).style.fill="989e5a";
+            break;
+            case (val.CPG12 >= 300 && val.CPG12 < 600):
+              document.getElementById("mapsvg").contentDocument.getElementById("reg-"+val.REG).style.fill="696642";
+            break;
+            case (val.CPG12 >= 600):
+              document.getElementById("mapsvg").contentDocument.getElementById("reg-"+val.REG).style.fill="27341e";
+            break;
+            default:
+              document.getElementById("mapsvg").contentDocument.getElementById("reg-"+val.REG).style.fill="FFFFFF";
+            break;
+            }
+          document.getElementById("mapsvg").contentDocument.getElementById("reg-"+val.REG).addEventListener("mousedown",function(){alert(this.id)},false);
+        });
+      }
+    });
+  }
 });
